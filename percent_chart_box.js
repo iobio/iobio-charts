d8f74acf54abaecec14be1eb0aea1ce2f80c613c
Anders Pitman <&ers@apitman.com>
2024-05-22 11:47:06 -0600

    Dev snapshot

--- percent_chart_box.js
+++ percent_chart_box.js
@@ -1,6 +1,19 @@
 import iobio from './lib/iobio.viz/iobio.viz.esm.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+
+async function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.setAttribute('src', src);
+    script.addEventListener('load', resolve);
+    script.addEventListener('error', reject);
+    document.body.appendChild(script);
+  });
+}
 
 function PercentChartBox() {
+
   const el = document.createElement('div');
   el.classList.add('percent');
   el.classList.add('panel');
@@ -9,26 +22,46 @@ function PercentChartBox() {
   const donutEl = document.createElement('div');
   el.appendChild(donutEl);
 
-  const pie = iobio.viz.pie;
+  (async () => {
 
-  const d3Pie = d3.layout.pie()
-    .sort(null);
+    const d3Pie = d3.pie()
+      .sort(null);
 
-  const chart = pie()
-    .radius(61)
-    .innerRadius(50)
-    .color( function(d,i) { if (i==0) return '#2d8fc1'; else return 'rgba(45,143,193,0.2)'; });
+    const chart = iobio.viz.pie()
+      .radius(61)
+      .innerRadius(50)
+      .color( function(d,i) { if (i==0) return '#2d8fc1'; else return 'rgba(45,143,193,0.2)'; });
 
-  const data = [1, 1, 2, 4];
+    const data = [1, 3];
 
-  const selection = d3.select(donutEl)
-    .datum(d3Pie(data));
+    const selection = d3.select(donutEl)
+      .datum(d3Pie(data));
 
-  setTimeout(() => {
-    chart(selection);
-  }, 1000);
+    setTimeout(() => {
+      chart(selection);
+    }, 500);
+
+  })();
 
   return el;
 }
+
+class PercentChartCE extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const dom = PercentChartBox();
+    this.appendChild(dom);
+  }
+
+  customFunc() {
+    console.log("WAT");
+  }
+}
+
+customElements.define('percent-chart-ce', PercentChartCE);
+
 
 export default PercentChartBox;
