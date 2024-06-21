fbd93f6d2e7a5accee408d5c7432cb151b74d406
Anders Pitman <&ers@apitman.com>
2024-06-21 11:11:32 -0600

    Connect read depth chart to data broker

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { PercentBoxElement, createPercentBox } from './percent_box.js';
 import { HistogramElement, createHistogram } from './histogram.js';
 import { DataBroker, DataBrokerElement } from './data_broker.js';
+import './coverage/src/BamView-WebComponent.js';
 
 
 export default {
