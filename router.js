3e71cefdc771bdbaa5a6be67fdb2fea427330704
Yang Qi <60582494+YangQi007@users.noreply.github.com>
2024-09-05 12:03:02 -0600

    Fixed the browser history stack and swap out the whole main content

--- router.js
+++ router.js
@@ -1,24 +1,26 @@
 
 function navigateTo(url) {
-    // Update the browser's history stack
-    history.pushState(null, null, url);
-    router();
-  }
+    if (window.location.pathname !== url) {
+        // Update the browser's history stack
+        history.pushState(null, null, url);
+        router();
+    }
+}
 
 
 function router() {
     // Get the current path
     const path = window.location.pathname;
 
+    const helpPage = document.getElementById('help-page');
+    const mainContent = document.getElementById('main-content');
+
     if (path === '/help') {
-        document.getElementById('help-page').style.display = 'block';
-        document.querySelector('.read-depth-container').style.visibility = 'hidden';
-        document.querySelector('.bottom-container').style.visibility = 'hidden';
+        helpPage.style.display = 'block';
+        mainContent.style.visibility = 'hidden';
     } else {
-        document.querySelector('.read-depth-container').style.visibility = 'visible';
-        document.querySelector('.bottom-container').style.visibility = 'visible';
-        // Ensure "Get Help" page is hidden
-        document.getElementById('help-page').style.display = 'none';
+        helpPage.style.display = 'none';
+        mainContent.style.visibility = 'visible';
     }
 }
 
@@ -27,9 +29,9 @@ function initRouter() {
     // Event listener for clicks on the "Get Help" link
     document.addEventListener('click', (event) => {
         if (event.target.matches('[data-link]')) {
-        event.preventDefault();
-        const url = event.target.getAttribute('data-link');
-        navigateTo(url);
+            event.preventDefault();
+            const url = event.target.getAttribute('data-link');
+            navigateTo(url);
         }
     });
 
