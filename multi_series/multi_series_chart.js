52b5b53e131ffb568015bd2d36a36dbee632ef66
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-06-05 15:03:54 -0600

    Initialize the basics
    Added the basic functions/skeleton to the chart class.

--- multi_series/multi_series_chart.js
+++ multi_series/multi_series_chart.js
@@ -17,9 +17,18 @@ class MultiSeriesChart {
         this.yScale = null;
 
         this.seriesTitles = [];
-        this.seriesSections = [];
-        this.seriesValues = [];
-        this.accumulatedSections = [];
+        this.seriesSegments = [];
+        /**
+         * An array of objects with the following properties:
+         * - title: The title of the series
+         * - bins: The bins of the series
+         * - mean: The mean of the series bins
+         * - sd: The standard deviation of the series bins
+         * - min: The minimum value of the series bins
+         * - max: The maximum value of the series bins
+         */
+        this.series = [];
+        this.accumulatedSegments = [];
 
         this._init(parent);
     }
@@ -46,11 +55,14 @@ class MultiSeriesChart {
             .attr("height", this.height)
             .attr("viewBox", [0, 0, this.width, this.height])
             .attr("class", "multi-series-chart");
+
+        // Append the SVG to the parent element
+        parent.appendChild(this.svg.node());
     }
 
     _initScale(bins) {
-        this.accumulatedSections = this._createAccumulatedMap(this.seriesSections); //Accumulate based just on the order provided
-        const totalLength = d3.sum(this.seriesSections, (d) => d.length);
+        //Accumulate based just on the order provided
+        const totalLength = d3.sum(this.seriesSegments[0], (d) => d.length);
 
         this.xScale = d3
             .scaleLinear()
@@ -59,7 +71,7 @@ class MultiSeriesChart {
 
         this.yMin = d3.min(bins, (d) => d.avgCoverage);
         this.yMean = d3.mean(bins, (d) => d.avgCoverage);
-        this.yMax = mean * 5; //Five times the mean, arbitrary but reasonable
+        this.yMax = this.yMean * 5; //Five times the mean, arbitrary but reasonable
 
         this.yScale = d3
             .scaleLinear()
@@ -73,23 +85,57 @@ class MultiSeriesChart {
 
         if (newYMin < this.yMin || newYMax > this.yMax) {
             this.yMin = newYMin;
-            this.yMax = newYMax;
-
-            this.yScale.domain([this.yMin, this.yMax]);
+            this.yMean = d3.mean(bins, (d) => d.avgCoverage);
+            this.yMax = this.yMean * 5; //Five times the mean, arbitrary but reasonable
+            this.yScale.domain([this.yMin, this.yMax]); // Update the yScale domain
 
             this.svg.selectAll("path").remove(); // Remove old paths
-            this.addSeries(this.seriesValues, this.seriesSections, this.seriesTitles); // Redraw the series with the new yScale
+            this._redrawSeries(this.series); // Redraw the series with the new yScale
+            return true;
         }
+        return false;
+    }
+
+    _redrawSeries(seriesValues) {
+        seriesValues.forEach((series, index) => {
+            d3.select(`#series-${index}`).remove();
+
+            const allBins = series.bins;
+
+            const dotPath = allBins
+                .map((d) => {
+                    const x = this.xScale(d.start);
+                    const y = this.yScale(d.avgCoverage);
+                    return `M${x},${y}h0`;
+                })
+                .join(" ");
+
+            this.svg
+                .append("path")
+                .attr("id", `series-${index}`)
+                .attr("d", dotPath)
+                .attr("stroke", series.color)
+                .attr("stroke-opacity", 0.8)
+                .attr("stroke-width", 1.5)
+                .attr("fill", "none")
+                .attr("stroke-linejoin", "round")
+                .attr("stroke-linecap", "round");
+        });
     }
 
     /**
      *     METHODS: PUBLIC
      */
 
-    addSeries(values, sections, title = "") {
+    addSeries(values, segments, title = "") {
+        if (!this.accumulatedSegments || this.accumulatedSegments.length === 0) {
+            this.accumulatedSegments = this._createAccumulatedMap(segments);
+            this.seriesSegments.push(segments);
+        }
+
         let allBins = [];
         Object.entries(values).forEach(([i, bins]) => {
-            let chr = this.accumulatedSections[sections[i].sn];
+            let chr = this.accumulatedSegments[segments[i].sn];
             let newBins = bins.map((bin) => {
                 bin.start = chr.start + bin.offset;
                 return bin;
@@ -103,50 +149,88 @@ class MultiSeriesChart {
             this._recheckYScale(allBins);
         }
 
-        allBins.forEach((d) => {
-            const xPos = this.xScale(d.start);
-            const yPos = this.yScale(d.avgCoverage);
-            let strokeColor = "gray";
-            let opacity = 1;
+        this.seriesTitles.push(title);
+        const index = this.series.length;
 
-            if (d.avgCoverage < this.yMean - 0.5 * this.yMean || d.avgCoverage > this.yMean + 0.5 * this.yMean) {
-                this.svg
-                    .append("path")
-                    .attr("d", `M${xPos},${yPos}h0`)
-                    .attr("stroke", strokeColor)
-                    .attr("stroke-opacity", opacity)
-                    .attr("stroke-width", 2)
-                    .attr("fill", "none")
-                    .attr("stroke-linejoin", "round")
-                    .attr("stroke-linecap", "round");
-            }
-        });
+        // Probably need 10 colors for the series
+        const colors = ["#C70000", "black", "#2D4B87", "orange", "teal", "pink", "green", "purple", "brown", "yellow"];
+        const color = colors[index] || "gray";
+
+        let newSeries = {
+            title: title,
+            bins: allBins,
+            mean: d3.mean(allBins, (d) => d.avgCoverage),
+            sd: d3.deviation(allBins, (d) => d.avgCoverage),
+            min: d3.min(allBins, (d) => d.avgCoverage),
+            max: d3.max(allBins, (d) => d.avgCoverage),
+            color: color,
+        };
+
+        this.series.push(newSeries);
+
+        const dotPath = allBins
+            .map((d) => {
+                const x = this.xScale(d.start);
+                const y = this.yScale(d.avgCoverage);
+                return `M${x},${y}h0`;
+            })
+            .join(" ");
+
+        this.svg
+            .append("path")
+            .attr("id", `series-${index}`)
+            .attr("d", dotPath)
+            .attr("stroke", newSeries.color)
+            .attr("stroke-opacity", () => {
+                if (newSeries.color === "#C70000") {
+                    return 1;
+                }
+                return 0.7;
+            })
+            .attr("stroke-width", 1.5)
+            .attr("fill", "none")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round");
     }
 
-    rescale() {
-        // TODO: Rescale the chart functionality
+    rescale(parent) {
+        let newWidth = parent.clientWidth;
+        let newHeight = parent.clientHeight;
+
+        if (newWidth !== this.width || newHeight !== this.height) {
+            this.width = newWidth;
+            this.height = newHeight;
+
+            this.svg.attr("width", this.width).attr("height", this.height).attr("viewBox", [0, 0, this.width, this.height]);
+
+            // Rescale the x and y axes
+            this.xScale.range([this.margin.left, this.width - this.margin.right]);
+            this.yScale.range([this.height - this.margin.bottom, this.margin.top]);
+
+            // Redraw the series with the new scales
+            this._redrawSeries(this.series);
+        }
     }
 
     /**
      *      HELPER FUNCTIONS: INTERNAL
      */
 
-    _createAccumulatedMap(header) {
-        //Segments Not Headers
+    _createAccumulatedMap(segments) {
         /**
-         * Takes in the header of relative coordinates and returns an object that has the accumulated positions for a linear view
+         * Takes in the segments and creates a map of the segments
          */
         let accumulatedMap;
 
         let total = 0;
         let i = 0;
-        accumulatedMap = header.reduce((acc, h) => {
-            acc[h.sn] = h;
-            acc[h.sn].start = total;
-            acc[h.sn].end = total + length;
-            acc[h.sn].position = i;
+        accumulatedMap = segments.reduce((acc, s) => {
+            acc[s.sn] = s;
+            acc[s.sn].start = total;
+            acc[s.sn].end = total + length;
+            acc[s.sn].position = i;
 
-            total += h.length;
+            total += s.length;
             i++;
             return acc;
         }, {});
